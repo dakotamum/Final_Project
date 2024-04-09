2d5b88cb3c2e1507a9972e5431488c891214b1d8
Dakota Mumford <dakotamum@gmail.com>
2024-04-08 22:46:00 -0600

    can now scale and rotate portals without trigger

--- Assets/MovePortalWithRaycast.cs
+++ Assets/MovePortalWithRaycast.cs
@@ -6,12 +6,13 @@ using UnityEngine.XR;
 
 public class MovePortalWithRaycast : MonoBehaviour
 {
-    public XRRayInteractor rayInteractor; // Assign in the inspector
-    public GameObject objectToMove; // Assign in the inspector or pass dynamically
+    public XRRayInteractor rayInteractor;
+    public GameObject objectToMove;
     public XRNode rightInputSource;
     public XRNode leftInputSource;
     private float scaleSpeed = 0.5f;
     private float rotationSpeed = 45f;
+    private bool isOnTable = true;
 
     void Update()
     {
@@ -27,38 +28,59 @@ public class MovePortalWithRaycast : MonoBehaviour
             {
                 Vector3 modifiedHitPoint = hit.point;
 
-                rightDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool isAPressed);
-                rightDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool isBPressed);
-                leftDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool isXPressed);
-                leftDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool isYPressed);
-                if (isAPressed)
-                {
-                    objectToMove.transform.localScale = objectToMove.transform.localScale - Vector3.one * scaleSpeed * Time.deltaTime;
-                }
-                if (isBPressed)
-                {
-                    objectToMove.transform.localScale = objectToMove.transform.localScale + Vector3.one * scaleSpeed * Time.deltaTime;
-                }
-
-                if (isXPressed)
-                {
-                    objectToMove.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
-                }
-                if (isYPressed)
-                {
-                    objectToMove.transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
-                }
-
                 if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Table"))
                 {
+                    isOnTable = true;
                     modifiedHitPoint.y = 1 - (1 - objectToMove.transform.localScale.y);
                 }
                 else if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Floor"))
                 {
+                    isOnTable = false;
                     modifiedHitPoint.y = -(1 - objectToMove.transform.localScale.y);
                 }
                 objectToMove.transform.position = modifiedHitPoint;
             }
         }
+        rightDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool isAPressed);
+        rightDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool isBPressed);
+        leftDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool isXPressed);
+        leftDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out bool isYPressed);
+        if (isAPressed)
+        {
+            objectToMove.transform.localScale = objectToMove.transform.localScale - Vector3.one * scaleSpeed * Time.deltaTime;
+            Vector3 newPosition = objectToMove.transform.position;
+            if (isOnTable)
+            {
+                newPosition.y = 1 - (1 - objectToMove.transform.localScale.y);
+            }
+            else
+            {
+                newPosition.y = -(1 - objectToMove.transform.localScale.y);
+            }
+            objectToMove.transform.position = newPosition;
+        }
+        if (isBPressed)
+        {
+            objectToMove.transform.localScale = objectToMove.transform.localScale + Vector3.one * scaleSpeed * Time.deltaTime;
+            Vector3 newPosition = objectToMove.transform.position;
+            if (isOnTable)
+            {
+                newPosition.y = 1 - (1 - objectToMove.transform.localScale.y);
+            }
+            else
+            {
+                newPosition.y = -(1 - objectToMove.transform.localScale.y);
+            }
+            objectToMove.transform.position = newPosition;
+        }
+        if (isXPressed)
+        {
+            objectToMove.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+        if (isYPressed)
+        {
+            objectToMove.transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+
     }
 }
